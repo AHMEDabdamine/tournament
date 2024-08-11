8f1c346cfd1950ee68f7c31f572074ec41f19ef5
AHMEDabdamine <140296603+AHMEDabdamine@users.noreply.github.com>
2024-08-11 01:58:49 +0100

    test

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
-    $host = "localhost";
-    $db_user = "filipa";#root
-    $db_password = "admin";#""
-    $db_name = "futebolamador";
+    $host = "s554ongw9quh1xjs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+    $db_user = "to2sgh3yxovvsi5x";#root
+    $db_password = "qjoxk2fic1mtep4d";#""
+    $db_name = "rvkic8vbiza3ftee";
 ?>
 
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'f5635fe76daadd8979a2ae2065b852918b7b96dd',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'f5635fe76daadd8979a2ae2065b852918b7b96dd',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
